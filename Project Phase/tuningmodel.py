2d6e2af5a0bbb4e09d6239fc8d142b714bfb48c0
Prasanga Niraula <078bct056@ioepc.edu.np>
2025-06-27 13:23:49 +0545

    Projectphase

---
+++ Project Phase/tuningmodel.py
@@ -0,0 +1,52 @@
+from torch import nn
+import torch.nn.functional as F
+
+class CustomCNN(nn.Module):
+    def __init__(self, num_classes, dropout_conv=0.25, dropout_fc=0.5):
+        super(CustomCNN, self).__init__()
+
+        self.conv_block1 = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(3),
+            nn.Dropout(dropout_conv)
+        )
+
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Dropout(dropout_conv)
+        )
+
+        self.conv_block3 = nn.Sequential(
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Dropout(dropout_conv)
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(128 * 5 * 5, 1024),
+            nn.ReLU(),
+            nn.BatchNorm1d(1024),
+            nn.Dropout(dropout_fc),
+            nn.Linear(1024, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.conv_block1(x)
+        x = self.conv_block2(x)
+        x = self.conv_block3(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
